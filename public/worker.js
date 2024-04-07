442bf4f952d54403ab590438b46a17a2bf9f956f
Kevin <klattkev@gmail.com>
2024-04-07 17:46:45 +0200

    asdasd

---
+++ public/worker.js
@@ -0,0 +1,72 @@
+
+import Noise from '../Noise';
+import MeshData from '../data/MeshData'
+import * as THREE from 'three'
+
+function generateMapData(mapChunkSize, noiseData, terrainData, center, useFalloff, falloffMap) {
+   
+    //console.log(center)
+    let newOffset = new THREE.Vector2()
+    newOffset.copy(noiseData.offset).add(center)
+
+    //console.log(newOffset)
+    const {noiseMap, minMaxHeight} = new Noise().generateNoiseMap(mapChunkSize, mapChunkSize, noiseData.seed, noiseData.noiseScale, noiseData.octaves, noiseData.persistance, noiseData.lacunarity, newOffset);
+
+    //console.log(noiseMap)
+    if(useFalloff) {
+        for (let y = 0; y < mapChunkSize; y++) {
+            for (let x = 0; x < mapChunkSize; x++) {
+                noiseMap[x][y] = THREE.MathUtils.clamp(noiseMap[x][y] - falloffMap[x][y], 0, 1)
+            }
+        }
+    }
+
+    return { noiseMap, minMaxHeight };
+}
+
+function generateTerrainMesh(heightMap, heightMultiplier = 15, levelOfDetail = 6) {
+
+    const width = heightMap.length;
+    const height = heightMap[0].length;
+    const topLeftX = (width - 1) / -2;
+    const topLeftZ = (height - 1) / 2;
+
+    const meshSimplificationIncrement = levelOfDetail == 0 ? 1 : levelOfDetail * 2;
+    const verticesPerLine = Math.ceil((width - 1) / meshSimplificationIncrement) + 1;
+
+    const meshData = new MeshData(verticesPerLine, verticesPerLine);
+    let vertexIndex = 0;
+
+    for (let y = 0; y < height; y += meshSimplificationIncrement) {
+        for (let x = 0; x < width; x += meshSimplificationIncrement) {
+            //let tempy = heightMap[x][y] * heightMultiplier
+            meshData.vertices[vertexIndex] = new THREE.Vector3(topLeftX + x, heightMap[x][y] * heightMultiplier, topLeftZ - y);
+            meshData.uvs[vertexIndex] = new THREE.Vector2(x / (width - 1), y / (height - 1));
+
+            if (x < width - 1 && y < height - 1) {
+                meshData.addTriangle(vertexIndex, vertexIndex + verticesPerLine + 1, vertexIndex + verticesPerLine);
+                meshData.addTriangle(vertexIndex + verticesPerLine + 1, vertexIndex, vertexIndex + 1);
+
+                //meshData.addTriangle(vertexIndex, vertexIndex + verticesPerLine, vertexIndex + verticesPerLine + 1);
+                //meshData.addTriangle(vertexIndex, vertexIndex + verticesPerLine + 1, vertexIndex + 1);
+            }
+
+            vertexIndex++;
+        }
+    }
+
+    return meshData;
+}
+
+self.onmessage = (event) => {
+
+    console.log("workerrrrrrrrrrr")
+
+    const { mapChunkSize, noiseData, terrainData, levelOfDetail, requestId, center, useFalloff, falloffMap  } = event.data;
+
+    let {noiseMap, minMaxHeight} = generateMapData(mapChunkSize, noiseData, terrainData, center, useFalloff, falloffMap);
+
+    let result = generateTerrainMesh(noiseMap, terrainData.meshHeightMulitplier, levelOfDetail)
+
+    self.postMessage({data: result, requestId: requestId, noiseData: noiseMap, minMaxHeight: minMaxHeight});
+};
